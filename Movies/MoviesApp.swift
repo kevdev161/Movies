54fc3de315599f994ee0e7e751aa6d1fbff668a3
Kevin M Jeggy <kevin.jeggy@strollby.com>
2025-05-31 14:29:37 +0530

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Kevin M Jeggy on 31/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
