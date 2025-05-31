f28da3575377f3d2044dfd04c8444df65dda1422
Kevin M Jeggy <kevin.jeggy@strollby.com>
2025-06-01 01:36:08 +0530

    refactor: [MoviesListScreen].  - favorites  - UI changes.

--- Movies/Listing/Screen/MoviesListScreen.swift
+++ Movies/Listing/Screen/MoviesListScreen.swift
@@ -12,7 +12,7 @@ struct MoviesListScreen: View {
     @StateObject private var favoritesManager = FavoritesManager()
 
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack(alignment: .leading) {
                 HStack {
                     Text("Movies")
@@ -32,39 +32,34 @@ struct MoviesListScreen: View {
                     .padding(10)
                     .background(Color(.systemGray6))
                     .cornerRadius(8)
-                    .padding(.horizontal)
                     .disableAutocorrection(true)
-                Button {
-                    viewModel.resetSearch()
-                } label: {
-                    Text("Done")
-                }
-
-                ScrollView {
-                    LazyVStack(alignment: .leading, spacing: 16) {
-                        ForEach(viewModel.movies.indices, id: \.self) { index in
-                            let movie = viewModel.movies[index]
-                            MovieListCard(movie: movie)
-                            Button(action: {
-                                favoritesManager.toggleFavorite(movie)
-                            }) {
-                                Image(systemName: favoritesManager.isFavorite(movie) ? "star.fill" : "star")
-                                    .foregroundColor(.yellow)
+                if !viewModel.isLoading, viewModel.movies.isEmpty, !viewModel.searchText.isEmpty {
+                    NoMoviesFoundView(searchText: viewModel.searchText) {
+                        viewModel.resetSearch()
+                    }.frame(maxHeight: .infinity)
+                } else {
+                    ScrollView(showsIndicators: false) {
+                        LazyVStack(alignment: .leading, spacing: 16) {
+                            ForEach(viewModel.movies.indices, id: \.self) { index in
+                                let movie = viewModel.movies[index]
+                                MovieListCard(movie: movie)
+                                    .onAppear {
+                                        if index == viewModel.movies.count - 1 {
+                                            viewModel.fetchMovies()
+                                        }
+                                    }
                             }
-                            .onAppear {
-                                if index == viewModel.movies.count - 1 {
-                                    viewModel.fetchMovies()
-                                }
+                            if viewModel.isLoading {
+                                ProgressView()
+                                    .frame(maxWidth: .infinity, alignment: .center)
                             }
-                        }
-                        if viewModel.isLoading {
-                            ProgressView()
-                                .frame(maxWidth: .infinity, alignment: .center)
-                        }
+                        }.environmentObject(favoritesManager)
                     }
                 }
-            }
-            .navigationTitle("Movies")
-        }.background(Color.white)
+            }.padding(.horizontal, 20)
+                .padding(.vertical, 32)
+                .scrollDismissesKeyboard(.immediately)
+                .ignoresSafeArea(.all, edges: .bottom)
+        }
     }
 }
